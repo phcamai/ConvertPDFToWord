60fdf479b04cb2c0d3236e877dd5c94a90c2dafc
Powerhouse Song <166808890+phcamai@users.noreply.github.com>
2024-05-10 00:22:13 +0800

    add to repo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Acrobat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertAcroPDApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String szPdfPathConst = "D:\\1.pdf";
+            String szDocxPathConst = "D:\\1.docx";
+            
+            //Initialize Acrobat by creating App object
+            AcroAppClass mApp = new AcroAppClass();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+Convert PDF To Word(docx) using Acrobat DC ActiveX.
+
+1. Install Adobe Acrobat DC(Pro).
+2. Load pdf file from argument.
+3. Convert and save docx file with same file name.
+
+
+
